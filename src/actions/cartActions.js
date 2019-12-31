69805491d213bb9c0dc1c93bbd81e8f9d862fa24
Josh <Joshua.horgen@gmail.com>
2019-12-30 17:06:04 -0800

    Initial commit

---
+++ src/actions/cartActions.js
@@ -0,0 +1,44 @@
+import { ADD_TO_CART, REMOVE_ITEM, SUB_QUANTITY, ADD_QUANTITY, SUB_SHIPPING, ADD_SHIPPING } from './action-types/cart-actions.js'
+
+//add cart action
+export const addToCart= (id)=>{
+    return{
+        type: ADD_TO_CART,
+        id
+    }
+}
+//remove item action
+export const removeItem=(id)=>{
+    return{
+        type: REMOVE_ITEM,
+        id
+    }
+}
+//subtract qt action
+export const subtractQuantity=(id)=>{
+    return{
+        type: SUB_QUANTITY,
+        id
+    }
+}
+//add qt action
+export const addQuantity=(id)=>{
+    return{
+        type: ADD_QUANTITY,
+        id
+    }
+}
+
+export const addShipping=(id)=>{
+    return{
+        type: ADD_SHIPPING,
+        id
+    }
+}
+
+export const subShipping=(id)=>{
+    return{
+        type: SUB_SHIPPING,
+        id
+    }
+}
---
+++ src/store.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// import App from './App';
+// import cartReducer from './reducers/cartReducer';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+//
+// const store = createStore(cartReducer);
+//
+// ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
