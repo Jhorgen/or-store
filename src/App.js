719c39199687e56a681d96560d7aff733cf4b29d
Josh <Joshua.horgen@gmail.com>
2020-01-22 00:03:12 -0800

    Add routes for each category.

--- src/App.js
+++ src/App.js
@@ -13,9 +13,29 @@ import Admin from './components/Admin'
 
 function App() {
 
-  const grips = "Grips";
-  const pedals = "Pedals";
-  const fox = "Fox";
+  const bars = 'Bars'
+  const frames = 'Frames'
+  const stems = 'Stems'
+  const tires = 'Tires'
+
+  const rims = 'Rims'
+  const hubs = 'Hubs'
+  const cranks = 'Cranks'
+  const forks = 'Forks'
+  const seats = 'Seats'
+  const pedals = "Pedals"
+  const sprockets = 'Sprockets'
+  const chains = 'Chains'
+  const pegs = 'Pegs'
+  const titanium = 'Titanium Hardware'
+  const gloves = 'Gloves'
+  const headsets = 'Headsets'
+  const grips = "Grips"
+
+
+
+
+  const fox = "Fox"
 
   return (
     <div className="App">
@@ -24,8 +44,34 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/" component={CategorySelect}/>
-            <Route render={props => ( <Shop name={grips}/>)} path="/grips"/>
+            <Route render={props => ( <Shop name={bars}/>)} path="/bars"/>
+            <Route render={props => ( <Shop name={frames}/>)} path="/frames"/>
+            <Route render={props => ( <Shop name={stems}/>)} path="/stems"/>
+            <Route render={props => ( <Shop name={tires}/>)} path="/tires"/>
+            <Route render={props => ( <Shop name={rims}/>)} path="/rims"/>
+            <Route render={props => ( <Shop name={hubs}/>)} path="/hubs"/>
+            <Route render={props => ( <Shop name={cranks}/>)} path="/cranks"/>
+            <Route render={props => ( <Shop name={forks}/>)} path="/forks"/>
+            <Route render={props => ( <Shop name={seats}/>)} path="/seats"/>
             <Route render={props => ( <Shop name={pedals}/>)} path="/pedals"/>
+            <Route render={props => ( <Shop name={sprockets}/>)} path="/sprockets"/>
+            <Route render={props => ( <Shop name={chains}/>)} path="/chains"/>
+            <Route render={props => ( <Shop name={pegs}/>)} path="/pegs"/>
+            <Route render={props => ( <Shop name={titanium}/>)} path="/titanium"/>
+            <Route render={props => ( <Shop name={gloves}/>)} path="/gloves"/>
+            <Route render={props => ( <Shop name={headsets}/>)} path="/headsets"/>
+            <Route render={props => ( <Shop name={grips}/>)} path="/grips"/>
+
+
+
+
+
+
+
+
+
+
+
 
             <Route path="/brands" component={BrandSelect}/>
             <Route render={props => ( <ShopByBrand brand={fox}/>)} path="/fox"/>
