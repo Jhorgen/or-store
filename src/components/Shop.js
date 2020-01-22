89bc9c31c033b54214fe077a246b339249bf44ee
Josh <Joshua.horgen@gmail.com>
2020-01-22 00:08:32 -0800

    Convert shop display to a mapped component (ShopItem), delayed commit.

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Row } from 'reactstrap'
 import { Link } from 'react-router-dom'
-import { addToCart, loadSelectedItemData, saveSelectedItemData } from './../actions/cartActions.js'
+import { addToCart, loadSelectedItemData, saveSelectedItemData, saveCartFromShop } from './../actions/cartActions.js'
+import ShopItem from './ShopItem'
 
 
 class Shop extends Component {
@@ -12,44 +13,27 @@ class Shop extends Component {
   }
 
   componentDidMount = () => {
+    console.log('typeof:', typeof(this.props.form.items[0].id));
     console.log("shop loaded items:", this.props.form.addedItems);
       let saveCartFromShop = this.props.form.addedItems
       let saveTotalFromShop = this.props.form.total
       console.log("shop:", 'no length');
-      this.props.saveSelectedItemData(this.props.name, saveCartFromShop, saveTotalFromShop)
+      if(this.props.form.addedItems.length > 0) {
+        console.log('length');
+        this.props.saveSelectedItemData(this.props.name, saveCartFromShop, saveTotalFromShop)
+      } else {
+        console.log('no length');
+        this.props.loadSelectedItemData(this.props.name)
+      }
   }
 
-
-
-  handleAddClick = (id) => {
-    this.props.addToCart(id);
-    console.log(this.props.test);
-    console.log(id);
-  }
-
-  findCat = () => {
-    console.log(this.props.form.name);
-  }
 
   render() {
     let items = this.props.form.items.length ?
     (
     this.props.form.items.map(item=>{
     return (
-      <div className="card m-3 test-hover" key={item.id}>
-        <div className="card-image text-center item-view-hover">
-          <Link to={'/item/' + item.title.split(' ').join('')} onClick={() => {this.checkLink(item)}}>
-          <img style={{height: '13rem'}} src={ require(`./../images/${item.image1}.jpg`)} alt={item.title}/>
-          <div><span className="card-title">{item.title}</span></div>
-          <span onClick={() => this.handleAddClick(item.id)}>View</span>
-        </Link>
-          <hr/>
-        </div>
-        <div className="card-content">
-          <p>{item.description}</p>
-          <p><b>Price: ${item.price}</b></p>
-        </div>
-      </div>
+      <ShopItem item={item} />
     )
   })
 )
