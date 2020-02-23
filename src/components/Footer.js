9cf6ee6e849eedc43547b09eccc83fae3d6f1f9b
Josh <Joshua.horgen@gmail.com>
2020-02-22 23:03:10 -0800

    Style footer to stay at the bottom of the page.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      <hr />
-      <div style={{background: "black"}}className="text-center">
+      <div style={{background: "black"}} className="text-center">
         <img className="mb-n4" style={{height: '13rem'}} src={ require(`./../images/orb-logo-footer.webp`)} alt={"The Oter Rim Bicycle Shop"}/>
         <p className="text-white">(503) 278-3235</p>
         <p className="text-white">10625 NE Halsey. Portland, OR 97220</p>
-
-        <br/>
-        <br/>
-      </div>
     </div>
   )
 }
