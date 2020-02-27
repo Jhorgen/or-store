6a09ee944247ef2541378d12e77f62939cbb0476
Josh <Joshua.horgen@gmail.com>
2020-02-27 12:39:31 -0800

    Add keys to Shop component for routing.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import OrbNavbar from './components/Navbar'
 import Footer from './components/Footer'
 import Shop from './components/Shop'
 import Cart from './components/Cart'
@@ -70,62 +70,61 @@ function App() {
     <StickyContainer>
       <BrowserRouter>
         <div>
-          <Navbar />
+          <OrbNavbar />
           <Switch>
             <Route exact path="/" component={CategorySelect}/>
-            <Route render={props => ( <Shop name={bars}/>)} path="/bars"/>
-            <Route render={props => ( <Shop name={frames}/>)} path="/frames"/>
-            <Route render={props => ( <Shop name={stems}/>)} path="/stems"/>
-            <Route render={props => ( <Shop name={tires}/>)} path="/tires"/>
-            <Route render={props => ( <Shop name={rims}/>)} path="/rims"/>
-            <Route render={props => ( <Shop name={hubs}/>)} path="/hubs"/>
-            <Route render={props => ( <Shop name={cranks}/>)} path="/cranks"/>
-            <Route render={props => ( <Shop name={forks}/>)} path="/forks"/>
-            <Route render={props => ( <Shop name={seats}/>)} path="/seats"/>
-            <Route render={props => ( <Shop name={pedals}/>)} path="/pedals"/>
-            <Route render={props => ( <Shop name={sprockets}/>)} path="/sprockets"/>
-            <Route render={props => ( <Shop name={chains}/>)} path="/chains"/>
-            <Route render={props => ( <Shop name={pegs}/>)} path="/pegs"/>
-            <Route render={props => ( <Shop name={titanium}/>)} path="/titanium"/>
-            <Route render={props => ( <Shop name={gloves}/>)} path="/gloves"/>
-            <Route render={props => ( <Shop name={headsets}/>)} path="/headsets"/>
-            <Route render={props => ( <Shop name={grips}/>)} path="/grips"/>
-
-            <Route render={props => ( <Shop name={brakes}/>)} path="/brakes"/>
-            <Route render={props => ( <Shop name={wheels}/>)} path="/wheels"/>
+            <Route render={props => ( <Shop name={bars} key='1'/>)} path="/bars"/>
+            <Route render={props => ( <Shop name={frames} key='2'/>)} path="/frames"/>
+            <Route render={props => ( <Shop name={stems} key='3'/>)} path="/stems"/>
+            <Route render={props => ( <Shop name={tires} key='4'/>)} path="/tires"/>
+            <Route render={props => ( <Shop name={rims} key='5'/>)} path="/rims"/>
+            <Route render={props => ( <Shop name={hubs} key='6'/>)} path="/hubs"/>
+            <Route render={props => ( <Shop name={cranks} key='7'/>)} path="/cranks"/>
+            <Route render={props => ( <Shop name={forks} key='8'/>)} path="/forks"/>
+            <Route render={props => ( <Shop name={seats} key='9'/>)} path="/seats"/>
+            <Route render={props => ( <Shop name={pedals} key='10'/>)} path="/pedals"/>
+            <Route render={props => ( <Shop name={sprockets} key='11'/>)} path="/sprockets"/>
+            <Route render={props => ( <Shop name={chains} key='12'/>)} path="/chains"/>
+            <Route render={props => ( <Shop name={pegs} key='13'/>)} path="/pegs"/>
+            <Route render={props => ( <Shop name={titanium} key='14'/>)} path="/titanium"/>
+            <Route render={props => ( <Shop name={gloves} key='15'/>)} path="/gloves"/>
+            <Route render={props => ( <Shop name={headsets} key='16'/>)} path="/headsets"/>
+            <Route render={props => ( <Shop name={grips} key='17'/>)} path="/grips"/>
+            <Route render={props => ( <Shop name={brakes} key='18'/>)} path="/brakes"/>
+            <Route render={props => ( <Shop name={wheels} key='19'/>)} path="/wheels"/>
 
 
             <Route path="/brands" component={BrandSelect}/>
-            <Route render={props => ( <ShopByBrand brand={colony} key='1'/>)} path="/colony-all"/>
-            <Route render={props => ( <ShopByBrand brand={snafu} key='2'/>)} path="/snafu-all"/>
-            <Route render={props => ( <ShopByBrand brand={fit} key='3'/>)} path="/fit-all"/>
-            <Route render={props => ( <ShopByBrand brand={tlc} key='4'/>)} path="/tlc-all"/>
-            <Route render={props => ( <ShopByBrand brand={primo} key='5'/>)} path="/primo-all"/>
-            <Route render={props => ( <ShopByBrand brand={pyc} key='6'/>)} path="/pyc-all"/>
-            <Route render={props => ( <ShopByBrand brand={volume} key='7'/>)} path="/volume-all"/>
-            <Route render={props => ( <ShopByBrand brand={blackOps} key='8'/>)} path="/blackops-all"/>
-            <Route render={props => ( <ShopByBrand brand={tallOrder} key='9'/>)} path="/tallorder-all"/>
-            <Route render={props => ( <ShopByBrand brand={kmc} key='10'/>)} path="/kmc-all"/>
-            <Route render={props => ( <ShopByBrand brand={odyssey} key='11'/>)} path="/odyssey-all"/>
+            <Route render={props => ( <ShopByBrand brand={colony} key='20'/>)} path="/colony-all"/>
+            <Route render={props => ( <ShopByBrand brand={snafu} key='21'/>)} path="/snafu-all"/>
+            <Route render={props => ( <ShopByBrand brand={fit} key='22'/>)} path="/fit-all"/>
+            <Route render={props => ( <ShopByBrand brand={tlc} key='23'/>)} path="/tlc-all"/>
+            <Route render={props => ( <ShopByBrand brand={primo} key='24'/>)} path="/primo-all"/>
+            <Route render={props => ( <ShopByBrand brand={pyc} key='25'/>)} path="/pyc-all"/>
+            <Route render={props => ( <ShopByBrand brand={volume} key='26'/>)} path="/volume-all"/>
+            <Route render={props => ( <ShopByBrand brand={blackOps} key='27'/>)} path="/blackops-all"/>
+            <Route render={props => ( <ShopByBrand brand={tallOrder} key='28'/>)} path="/tallorder-all"/>
+            <Route render={props => ( <ShopByBrand brand={kmc} key='29'/>)} path="/kmc-all"/>
+            <Route render={props => ( <ShopByBrand brand={odyssey} key='30'/>)} path="/odyssey-all"/>
 
-            <Route render={props => ( <ShopByBrand brand={shadowConspiracy} key='12'/>)} path="/shadowconspiracy-all"/>
-            <Route render={props => ( <ShopByBrand brand={sandm} key='13'/>)} path="/s&m-all"/>
-            <Route render={props => ( <ShopByBrand brand={total} key='14'/>)} path="/total-all"/>
-            <Route render={props => ( <ShopByBrand brand={division} key='15'/>)} path="/division-all"/>
-            <Route render={props => ( <ShopByBrand brand={demolition} key='16'/>)} path="/demolition-all"/>
-            <Route render={props => ( <ShopByBrand brand={fist} key='17'/>)} path="/fist-all"/>
-            <Route render={props => ( <ShopByBrand brand={sunday} key='18'/>)} path="/sunday-all"/>
-            <Route render={props => ( <ShopByBrand brand={odi} key='19'/>)} path="/odi-all"/>
+            <Route render={props => ( <ShopByBrand brand={shadowConspiracy} key='31'/>)} path="/shadowconspiracy-all"/>
+            <Route render={props => ( <ShopByBrand brand={sandm} key='32'/>)} path="/s&m-all"/>
+            <Route render={props => ( <ShopByBrand brand={total} key='33'/>)} path="/total-all"/>
+            <Route render={props => ( <ShopByBrand brand={division} key='34'/>)} path="/division-all"/>
+            <Route render={props => ( <ShopByBrand brand={demolition} key='35'/>)} path="/demolition-all"/>
+            <Route render={props => ( <ShopByBrand brand={fist} key='36'/>)} path="/fist-all"/>
+            <Route render={props => ( <ShopByBrand brand={sunday} key='37'/>)} path="/sunday-all"/>
+            <Route render={props => ( <ShopByBrand brand={odi} key='38'/>)} path="/odi-all"/>
 
-            <Route render={props => ( <ShopByBrand brand={bsd} key='20'/>)} path="/bsd-all"/>
-            <Route render={props => ( <ShopByBrand brand={fsa} key='21'/>)} path="/fsa-all"/>
-            <Route render={props => ( <ShopByBrand brand={gsport} key='22'/>)} path="/gsport-all"/>
-            <Route render={props => ( <ShopByBrand brand={kore} key='23'/>)} path="/kore-all"/>
-            <Route render={props => ( <ShopByBrand brand={federal} key='24'/>)} path="/federal-all"/>
-            <Route render={props => ( <ShopByBrand brand={stranger} key='25'/>)} path="/stranger-all"/>
-            <Route render={props => ( <ShopByBrand brand={academy} key='26'/>)} path="/academy-all"/>
-            <Route render={props => ( <ShopByBrand brand={maxxis} key='27'/>)} path="/maxxis-all"/>
-            <Route render={props => ( <ShopByBrand brand={kool} key='28'/>)} path="/kool-all"/>
+            <Route render={props => ( <ShopByBrand brand={bsd} key='39'/>)} path="/bsd-all"/>
+            <Route render={props => ( <ShopByBrand brand={fsa} key='40'/>)} path="/fsa-all"/>
+            <Route render={props => ( <ShopByBrand brand={gsport} key='41'/>)} path="/gsport-all"/>
+            <Route render={props => ( <ShopByBrand brand={kore} key='42'/>)} path="/kore-all"/>
+            <Route render={props => ( <ShopByBrand brand={federal} key='43'/>)} path="/federal-all"/>
+            <Route render={props => ( <ShopByBrand brand={stranger} key='44'/>)} path="/stranger-all"/>
+            <Route render={props => ( <ShopByBrand brand={academy} key='45'/>)} path="/academy-all"/>
+            <Route render={props => ( <ShopByBrand brand={maxxis} key='46'/>)} path="/maxxis-all"/>
+            <Route render={props => ( <ShopByBrand brand={kool} key='47'/>)} path="/kool-all"/>
 
 
             <Route path="/:brand/:title/" component={ItemView}/>
