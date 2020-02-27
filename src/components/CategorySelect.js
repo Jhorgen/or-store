28cbb460cb1533e1195c947ffc3757753330d825
Josh <Joshua.horgen@gmail.com>
2020-02-27 12:46:05 -0800

    Remove columns for better responsiveness.

--- src/components/CategorySelect.js
+++ src/components/CategorySelect.js
@@ -49,11 +49,11 @@ class CategorySelect extends Component {
         <div style={{display: this.state.spinner}} className='text-center mt-2 mb-5'>
           <Spinner color="info" />
         </div>
-        <Row style={{display: this.state.itemDisplay}} className="justify-content-around m-3">
+        <div className='container'>
+        <Row style={{display: this.state.itemDisplay}} className="justify-content-center m-3">
 
-          <Row className='m-3 cat-row justify-content-center'>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Bars</h4>
                 <Link
@@ -62,9 +62,9 @@ class CategorySelect extends Component {
                   <img className='cat-img' style={{height: '13rem', width: '100%'}} src={ require(`./../images/colony-bio-mech-bars-black.jpg`)} alt="Bars" title="Bars"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2' style={{padding: '0'}}>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Stems</h4>
                 <Link
@@ -73,9 +73,9 @@ class CategorySelect extends Component {
                   <img className='cat-img' style={{height: '13rem',  width: '100%'}} src={ require(`./../images/colony-squareback-rainbow.jpg`)} alt="Bars" title="Bars"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Frames</h4>
                 <Link
@@ -84,10 +84,10 @@ class CategorySelect extends Component {
                   <img className='cat-img' style={{height: '13rem',  width: '100%'}} src={ require(`./../images/testframe.jpg`)} alt="Bars" title="Bars"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Tires</h4>
                 <Link
@@ -96,11 +96,9 @@ class CategorySelect extends Component {
                   <img style={{height: '12rem',  width: '100%'}} src={ require(`./../images/tires.jpg`)} alt="Bars" title="Bars"/>
                 </Link>
               </div>
-            </Col>
-          </Row>
+            </div>
 
-          <Row className='m-3 cat-row justify-content-center'>
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Rims</h4>
                 <Link to='/rims'
@@ -108,9 +106,9 @@ class CategorySelect extends Component {
                   <img className='cat-img' style={{height: '13rem',  width: '100%'}} src={ require(`./../images/division-city-wide-rim-black.jpg`)} alt="Bars" title="Bars"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Hubs</h4>
                 <Link
@@ -119,9 +117,9 @@ class CategorySelect extends Component {
                   <img className='cat-img' style={{height: '13rem',  width: '100%'}} src={ require(`./../images/bsd-swerve-cassette-hub-black.jpg`)} alt="hubs" title="hubs"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Cranks</h4>
                 <Link
@@ -130,9 +128,9 @@ class CategorySelect extends Component {
                   <img className='cat-img' style={{height: '13rem',  width: '100%'}} src={ require(`./../images/fit-indent-crank-black.jpg`)} alt="Cranks" title="Cranks"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Forks</h4>
                 <Link
@@ -141,11 +139,9 @@ class CategorySelect extends Component {
                   <img className='cat-img' style={{height: '13rem',  width: '100%'}} src={ require(`./../images/colony-sweet-tooth-fork-black.jpg`)} alt="Forks" title="Forks"/>
                 </Link>
               </div>
-            </Col>
-          </Row>
+            </div>
 
-          <Row className='m-3 cat-row justify-content-center'>
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Seats</h4>
                 <Link
@@ -154,9 +150,9 @@ class CategorySelect extends Component {
                   <img className='cat-img' style={{height: '13rem', width: '100%'}} src={ require(`./../images/fit-stitch-pivotal-seat-blackdenim.jpg`)} alt="Seats" title="Seats"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Pedals</h4>
                 <Link
@@ -165,9 +161,9 @@ class CategorySelect extends Component {
                   <img className='cat-img' style={{height: '13rem', width: '100%'}} src={ require(`./../images/fit-mack-alloy-pedal-oilslick.jpg`)} alt="Pedals" title="Pedals"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Sprockets</h4>
                 <Link
@@ -176,9 +172,9 @@ class CategorySelect extends Component {
                   <img className='cat-img' style={{height: '13rem',  width: '100%'}} src={ require(`./../images/colony-blaster-sprocket-black.jpg`)} alt="Sprockets" title="Sprockets"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Chains</h4>
                 <Link
@@ -187,11 +183,9 @@ class CategorySelect extends Component {
                   <img style={{height: '13rem',  width: '100%'}} src={ require(`./../images/kmc-z410-chain-gold.jpg`)} alt="kmc-z410-chain-gold" title="Chains"/>
                 </Link>
               </div>
-            </Col>
-          </Row>
+            </div>
 
-          <Row className='m-3 cat-row justify-content-center'>
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Pegs</h4>
                 <Link
@@ -200,9 +194,9 @@ class CategorySelect extends Component {
                   <img style={{height: '13rem',  width: '100%'}} src={ require(`./../images/colony-anyway-peg-black3.jpg`)} alt="Pegs" title="Pegs"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Titanium hardware</h4>
                 <Link
@@ -211,9 +205,9 @@ class CategorySelect extends Component {
                   <img style={{height: '13rem',  width: '100%'}} src={ require(`./../images/titanium.jpg`)} alt="Titanium hardware" title="Titanium hardware"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Gloves</h4>
                 <Link
@@ -222,9 +216,9 @@ class CategorySelect extends Component {
                   <img style={{height: '13rem',  width: '100%'}} src={ require(`./../images/fist-miami-glove.jpg`)} alt="Gloves" title="Gloves"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Headsets</h4>
                 <Link
@@ -233,11 +227,9 @@ class CategorySelect extends Component {
                   <img style={{height: '13rem',  width: '100%'}} src={ require(`./../images/primo-integrated-headset-turquoise.jpg`)} alt="Headsets" title="Headsets"/>
                 </Link>
               </div>
-            </Col>
-          </Row>
+            </div>
 
-          <Row className='m-3 cat-row justify-content-center'>
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Grips</h4>
                 <Link
@@ -246,9 +238,9 @@ class CategorySelect extends Component {
                   <img style={{height: '13rem',  width: '100%'}} src={ require(`./../images/odi-longneck-soft-grip-black.jpg`)} alt="Grips" title="Grips"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Brakes</h4>
                 <Link
@@ -257,9 +249,9 @@ class CategorySelect extends Component {
                   <img style={{height: '13rem',  width: '100%'}} src={ require(`./../images/colony-brethren-brake-set-black.jpg`)} alt="Grips" title="Grips"/>
                 </Link>
               </div>
-            </Col>
+            </div>
 
-            <Col xs='2'>
+            <div className='card m-3'>
               <div className='test-hover' style={{border: '1px solid beige'}}>
                 <h4 className="text-center category-select">Complete Wheels</h4>
                 <Link
@@ -268,10 +260,10 @@ class CategorySelect extends Component {
                   <img style={{height: '13rem',  width: '100%'}} src={ require(`./../images/colony-pintour-freecoaster-wheel-black.jpg`)} alt="Grips" title="Grips"/>
                 </Link>
               </div>
-            </Col>
-          </Row>
+            </div>
 
         </Row>
+      </div>
         {this.state.footerDisplay}
       </div>
     )
