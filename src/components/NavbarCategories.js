9fd57161503707de58e9b594c9919f894a25b81f
Josh <Joshua.horgen@gmail.com>
2020-02-27 12:47:54 -0800

    Style navbar dropdowns.

--- src/components/NavbarCategories.js
+++ src/components/NavbarCategories.js
@@ -4,7 +4,6 @@ import { Row, Col, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from '
 import { Sticky } from 'react-sticky';
 
 
-
 class NavbarCategories extends Component {
   constructor(props) {
     super(props)
@@ -34,39 +33,39 @@ class NavbarCategories extends Component {
 
   render() {
     return(
-      <div>
-          <Col>
+      <div className='mb-2'>
+        <Col>
           <Dropdown className="d-inline-block" onMouseOver={this.onMouseEnter} onMouseLeave={this.onMouseLeave} isOpen={this.state.dropdownOpen} toggle={this.toggle}>
             <DropdownToggle
             tag="span"
             data-toggle="dropdown"
             aria-expanded={this.state.dropdownOpen}
             style={{color: 'white'}}>
-              Categories
+              <span className='navbar-font'>Categories</span>
             </DropdownToggle>
             <DropdownMenu>
-              <div onClick={() => this.toggle}><Link to='/bars'>Bars</Link></div>
-              <div onClick={() => this.toggle}><Link to='/brakes'>Brakes</Link></div>
-              <div onClick={() => this.toggle}><Link to='/hubs'>Hubs</Link></div>
-              <div onClick={() => this.toggle}><Link to='/rims'>Rims</Link></div>
-              <div onClick={() => this.toggle}><Link to='/pedals'>Pedals</Link></div>
-              <div onClick={() => this.toggle}><Link to='/sprockets'>Sprockets</Link></div>
-              <div onClick={() => this.toggle}><Link to='/chains'>Chains</Link></div>
-              <div onClick={() => this.toggle}><Link to='/seats'>Seats</Link></div>
-              <div onClick={() => this.toggle}><Link to='/forks'>Forks</Link></div>
-              <div onClick={() => this.toggle}><Link to='/headsets'>Heatsets</Link></div>
-              <div onClick={() => this.toggle}><Link to='/gloves'>Gloves</Link></div>
-              <div onClick={() => this.toggle}><Link to='/pegs'>Pegs</Link></div>
-              <div onClick={() => this.toggle}><Link to='/grips'>Grips</Link></div>
-              <div onClick={() => this.toggle}><Link to='/titanium'>Titanium Hardware</Link></div>
-              <div onClick={() => this.toggle}><Link to='/cranks'>Cranks</Link></div>
-              <div onClick={() => this.toggle}><Link to='/stems'>Stems</Link></div>
-              <div onClick={() => this.toggle}><Link to='/tires'>Tires</Link></div>
-              <div onClick={() => this.toggle}><Link to='/wheels'>Wheels</Link></div>
-              <div onClick={() => this.toggle}><Link to='/frames'>Frames</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/bars'>Bars</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/brakes'>Brakes</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/hubs'>Hubs</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/rims'>Rims</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/pedals'>Pedals</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/sprockets'>Sprockets</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/chains'>Chains</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/seats'>Seats</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/forks'>Forks</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/headsets'>Heatsets</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/gloves'>Gloves</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/pegs'>Pegs</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/grips'>Grips</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/titanium'>Titanium Hardware</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/cranks'>Cranks</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/stems'>Stems</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/tires'>Tires</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/wheels'>Wheels</Link></div>
+              <div style={{fontWeight: 'bold', fontSize: '17px', padding: '4px'}} className='text-center' onClick={() => this.toggle}><Link to='/frames'>Frames</Link></div>
             </DropdownMenu>
           </Dropdown>
-          </Col>
+        </Col>
       </div>
     )
   }
