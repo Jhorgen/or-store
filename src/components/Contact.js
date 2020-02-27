ab9bcad51b5d3444c4c4cd3166b1a8d4202fba7d
Josh <Joshua.horgen@gmail.com>
2020-02-27 12:46:30 -0800

    Create Contact component.

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Component extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contact us at orbbikeshop.support@gmail.com</h2>
+        //create orbbikeshop.sales@gmail.com for sales list
+      
+      </div>
+    )
+  }
+}
