1a518deae7bc5312add49568b0c75c000581ea87
Josh <Joshua.horgen@gmail.com>
2020-03-02 23:49:24 -0800

    Create a component for search results.

---
+++ src/components/SearchResults.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class SearchResults extends Component {
+  constructor(props) {
+    super(props) {
+      this.state = {
+        results: ''
+      }
+    }
+  }
+
+  render() {
+    
+    return (
+      <div>
+        <p>Test</p>
+      </div>
+    );
+  }
+}
+
+export default SearchResults
