a12f80039e6e016a1e03906c40cd841faaa12f24
Josh <Joshua.horgen@gmail.com>
2020-03-07 18:35:36 -0800

    Add fonts, fa icons, styles, etc. to footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,34 @@
 import React from 'react'
 import { Row, Col } from 'reactstrap'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faFacebookF} from "@fortawesome/free-brands-svg-icons"
+import {faInstagram} from "@fortawesome/free-brands-svg-icons"
 
 
 const Footer = () => {
   return (
     <Row style={{background: "black", margin: '0'}} className="text-center align-items-center">
       <Col className='mt-4'>
-        <Link className='text-light' to='/'><p>About us</p></Link>
-        <Link className='text-light' to='/'><p>Contact us</p></Link>
-        <Link className='text-light' to='/'><p>Return policy</p></Link>
+        <Link className='text-light add-font' to='/'><p>About us</p></Link>
+        <Link className='text-light add-font' to='/'><p>Contact us</p></Link>
+        <Link className='text-light add-font' to='/'><p>Return policy</p></Link>
       </Col>
 
-      <Col className='mt-n5'>
+      <Col className='mt-n5 footer-logo-toggle'>
         <img className="mb-n5" style={{height: '13rem'}} src={ require(`./../images/orb-logo-footer.webp`)} alt={"The Oter Rim Bicycle Shop"}/>
-        <p className="text-white">(503) 278-3235</p>
-        <p className="text-white">10625 NE Halsey. Portland, OR 97220</p>
+        <p className="text-white add-font" style={{margin: '0', marginBottom: '5px'}}>(503) 278-3235</p>
+        <p className="text-white add-font">10625 NE Halsey. Portland, OR 97220</p>
       </Col>
 
       <Col className='text-light mt-4 mb-2'>
-        <div className='pb-2'>
-          <img className='pr-2' src={ require(`./../images/garage-icon.webp`)} alt={"The Oter Rim Bicycle Shop"}/>
-          <img src={ require(`./../images/cycle-dog-icon.webp`)} alt={"The Oter Rim Bicycle Shop"}/>
-        </div>
-        <div className='pb-2'>
-          <img className='pr-2' src={ require(`./../images/acima-icon.webp`)} alt={"The Oter Rim Bicycle Shop"}/>
-          <img src={ require(`./../images/shimano-icon.webp`)} alt={"The Oter Rim Bicycle Shop"}/>
+        <div>
+          <a className='mr-3' href="https://www.facebook.com/outerrimbicycles/" target="about_blank" title="Our Facebook page">
+            <FontAwesomeIcon className='text-secondary' style={{fontSize: '1.7rem'}} icon={faFacebookF} />
+          </a>
+          <a href="https://www.instagram.com/outerrimbicycles/" target="about_blank" title="Our Instagram page">
+            <FontAwesomeIcon className='text-secondary' style={{fontSize: '1.65rem'}} icon={faInstagram} />
+          </a>
         </div>
       </Col>
     </Row>
