259a8a71b7732bbd28b1319ba07bf81282d331f6
Josh <Joshua.horgen@gmail.com>
2020-03-16 12:16:53 -0700

    Add contact & about components.

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h5>Contact us at TheOuterRimShop.inquiry@gmail.com</h5>
+      </div>
+    )
+  }
+}
+
+export default About;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-class Component extends Component {
+class Contact extends Component {
   render() {
     return (
       <div>
-        <h2>Contact us at TheOuterRimShop.Inquiry@gmail.com</h2>
+        <h2>Contact us at TheOuterRimShop.inquiry@gmail.com</h2>
       </div>
     )
   }
