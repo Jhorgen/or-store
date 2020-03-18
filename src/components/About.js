ff2cdc5cff04fe85925fbc0e5782197670d1b53e
Josh <Joshua.horgen@gmail.com>
2020-03-17 22:44:45 -0700

    Finish styling about / contact components.

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import { Row } from 'reactstrap'
+import Footer from './Footer'
 
-class About extends Component {
-  render() {
+const About = () => {
     return (
-      <div>
-        <h5>Contact us at TheOuterRimShop.inquiry@gmail.com</h5>
+      <div className='footer-control'>
+        <div className='container'>
+        <Row className='justify-content-center mt-2'>
+        <img style={{width: '100%', height: 'fit-content'}} src={require(`./../images/orb-about.jpeg`)} alt="Outer Rim Storefront" title="Outer Rim Storefront"/>
+        </Row>
+        <div>
+          <hr style={{width: '90%'}}/>
+          <h5 className='text-center add-font'>Who We Are</h5>
+          <hr style={{width: '75%'}}/>
+          <p className='add-font'><b>The Outer Rim</b> is Portland’s family owned and operated bike shop. Our goal is to meet your individual bike needs and offer superior customer service. We stock everything from BMX and Mountain to Commuter and E-bikes. Our BMX inventory is now available online through our web store. Check out our popular products by Snafu, Colony, and other hard to come by parts!</p>
+        </div>
+      </div>
+      <Footer/>
       </div>
     )
   }
-}
 
 export default About;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import { Row } from 'reactstrap'
+import Footer from './Footer'
 
-class Contact extends Component {
-  render() {
+const Contact = () => {
     return (
-      <div>
-        <h2>Contact us at TheOuterRimShop.inquiry@gmail.com</h2>
+      <div className='footer-control'>
+        <div className='container'>
+        <Row className='justify-content-center mt-2'>
+        <img style={{width: '100%', height: 'fit-content'}} src={require(`./../images/orb-about.jpeg`)} alt="Outer Rim Storefront" title="Outer Rim Storefront"/>
+        </Row>
+        <div>
+          <hr style={{width: '90%'}}/>
+          <h5 className='text-center add-font'>Contact Us</h5>
+          <hr style={{width: '75%'}}/>
+            <p className='add-font'>For questions or product information, contact us at <b>TheOuterRimShop.inquiry@gmail.com</b>.</p>
+        </div>
+
+        <div className='mt-5 text-left'>
+          <hr style={{width: '90%'}}/>
+          <h5 className='text-center add-font'>Returns</h5>
+          <hr style={{width: '75%'}}/>
+            <p className='add-font'>Returns are accepted within 30 days if the product is unopened & unused. Please contact us and include the following information:</p>
+            <ul>
+              <li>First & last name.</li>
+              <li>Date of purchase.</li>
+              <li>Products to be returned.</li>
+            </ul>
+        </div>
+      </div>
+      <Footer/>
       </div>
     )
   }
-}
+
+export default Contact;
